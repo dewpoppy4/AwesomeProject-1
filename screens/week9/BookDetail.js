9696fc8b16ca66fb93cd46dced408e3bf655bdec
dewpoppy4 <96884356+dewpoppy4@users.noreply.github.com>
2024-02-01 15:57:32 +0700

    WEEK11

--- screens/week9/BookDetail.js
+++ screens/week9/BookDetail.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useLayoutEffect, useState } from "react";
-import { Alert, Image, Text, TouchableOpacity, View } from "react-native";
+import { Alert, Image, Modal, Text, TouchableOpacity, View } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import BookStorage from "../../storages/BookStorage";
+import ImageViewer from "react-native-image-zoom-viewer";
+import File from "../../helpers/File";
 
 export default function BookDetail() {
     const route = useRoute();
     const { item } = route.params;
     const [book, setBook] = useState(item);
-
+    const [modalVisible, setModalVisible] = useState(false);
     const confirmDelete = () => {
         return Alert.alert(
             "ยืนยันการลบ?",
@@ -27,10 +29,11 @@ export default function BookDetail() {
         navigation.navigate("Book");
     };
 
-    useEffect(async () => {
+    const onLoad = async()=>{
         let b = await BookStorage.readItemDetail(item);
         setBook(b);
-    }, []);
+    };
+    useEffect(async () => { onLoad(); }, []);
 
     const navigation = useNavigation();
     useLayoutEffect(() => {
@@ -50,14 +53,24 @@ export default function BookDetail() {
 
     return (
         <View style={{ backgroundColor: "white", padding: 20, flex: 1 }} >
+            <TouchableOpacity onPress={() => {setModalVisible(true);}} >
             <View style={{ flexDirection: "row" }}>
                 <Image style={{ flex: 1, resizeMode: "contain", aspectRatio: 1 / 1 }} source={{ uri:book.image }} />
             </View>
+            </TouchableOpacity>
             <Text style={{ fontSize: 20, height: 70, marginVertical: 10 }}> {book.name} </Text>
+            
             <View style={{ flexDirection: "row" }}>
                 <Text style={{ color: "green", fontSize: 20 }}>{book.price}</Text>
                 <Text style={{ paddingTop: 6 }}> บาท</Text>
             </View>
+            <Modal visible={modalVisible} transparent={true} onRequestClose={() => { setModalVisible(false); }} >
+                <ImageViewer imageUrls={[{ url: book.image, props: { } }]}
+                    enableSwipeDown={true}
+                    onCancel={()=>{ console.log("SwipeDown"); setModalVisible(false); }}
+                    onSave={(uri)=>{ File.download(uri); alert("Save File Already!!!!"); }}
+                    />
+            </Modal>
         </View>
     );
 }
--- screens/week9/BookForm.js
+++ screens/week9/BookForm.js
@@ -3,8 +3,21 @@ import { View, KeyboardAvoidingView, ScrollView, Text, Button, TextInput } from 
 import { FontAwesome } from "@expo/vector-icons";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import BookStorage from "../../storages/BookStorage";
+import UploadArea from "../../components/week11/UploadArea";
 
 export default function BookForm() {
+    const onLoad = async()=> {
+        if (item) {
+          let book = await BookStorage.readItemDetail(item);
+          setId(book.id);
+          setName(book.name);
+          setPrice(book.price.toString());
+          setImage(book.image);
+        }
+      };
+      useEffect(() => { onLoad(); }, []);
+    
+    
     const [id, setId] = useState("_" + Math.random().toString(36).substring(2, 9));
     const [name, setName] = useState("");
     const [price, setPrice] = useState("");
@@ -13,18 +26,16 @@ export default function BookForm() {
     const { item } = route.params;
     const navigation = useNavigation();
     useLayoutEffect(() => { navigation.setOptions({ title: item ? "edit" : "create" }); }, [navigation]);
-   
-    useEffect(async() => {
-        if (item) {
-            let book = await BookStorage.readItemDetail(item);
-            setId(book.id);
-            setName(book.name);
-            setPrice(book.price.toString());
-            setImage(book.image);
-        }
-    }, []);
 
-
+    const saveBook = async () => {
+        //A NEW ITEM
+        let new_data = { id: id, name: name, price: price, image: image };
+        //SAVE
+        await BookStorage.writeItem(new_data);
+        //REDIRECT TO
+        navigation.navigate("Book");
+      };
+    
 
     return (
         <KeyboardAvoidingView style={{ flex: 1, padding: 20 }}>
@@ -35,8 +46,10 @@ export default function BookForm() {
                 <TextInput placeholder="Enter price ..." value={price} onChangeText={(text) => setPrice(text)} />
                 <Text>ลิงค์รูปภาพ</Text>
                 <TextInput placeholder="Enter image URL ..." value={image} onChangeText={(text) => setImage(text)} />
+
+                <UploadArea image={image} setImage={setImage} />
             </ScrollView>
-            <Button title="SAVE" color="tomato" />
+            <Button title="SAVE" color="tomato" onPress={saveBook} />
 
         </KeyboardAvoidingView>
     );
